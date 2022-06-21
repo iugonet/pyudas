7aa4f5159e560f2b7456255a4df110934dc10d72
iugonet <iugonet@iugonet0.nipr.ac.jp>
2022-06-21 16:24:14 +0900

    Modified.

--- iugonet/__init__.py
+++ iugonet/__init__.py
@@ -1,5 +1,6 @@
 from .ground.gmag_nipr import gmag_nipr
 from .ground.gmag_nipr_induction import gmag_nipr_induction
 from .ground.ask_nipr import ask_nipr
+from .ground.gmag_wdc import gmag_wdc
 from .ground.eiscat import eiscat
 from .ground.meteor_rish import meteor_rish
