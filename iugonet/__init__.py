8232805e80ee3ecd9f9693f0fede80f09bb4998c
ytanaka <ytanaka@nipr.ac.jp>
2024-12-14 20:37:38 +0900

    Modified __init__.py.

--- iugonet/__init__.py
+++ iugonet/__init__.py
@@ -3,6 +3,7 @@ from iugonet.ground.gmag_nipr_induction import gmag_nipr_induction
 from iugonet.ground.ask_nipr import ask_nipr
 from iugonet.ground.gmag_wdc import gmag_wdc
 from iugonet.ground.eiscat import eiscat
+from iugonet.ground.eiscat_vief import eiscat_vief
 from iugonet.ground.elf_hokudai import elf_hokudai
 from iugonet.ground.kyushugcm import kyushugcm
 from iugonet.ground.asi_nipr import asi_nipr
