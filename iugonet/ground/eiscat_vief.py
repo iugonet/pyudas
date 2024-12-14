4b4ef02f299c0071309390cc3a97e17311332a1c
ytanaka <ytanaka@nipr.ac.jp>
2024-12-14 22:17:12 +0900

    Modified eiscat_vief.py for test.

--- iugonet/ground/eiscat_vief.py
+++ iugonet/ground/eiscat_vief.py
@@ -27,7 +27,7 @@ def eiscat_vief(
     #===== Set parameters (1) =====#
     file_format = 'cdf'
 #    remote_data_dir = 'http://pc115.seg20.nipr.ac.jp/www/eiscatdata/cdf/'
-    remote_data_dir = 'http://polaris.nipr.ac.jp/~ytanaka/data/eiscat/'
+    remote_data_dir = 'http://polaris.nipr.ac.jp/~iugonet/data/eiscat/'
     local_path = 'nipr/eiscat/'
     prefix = 'eiscat_vief'
     file_res = 3600. * 24
