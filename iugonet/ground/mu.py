a7506c03cff92691a74cebb33acc7ea784213d3c
Atsuki Shinbori <shinbori@isee.nagoya-u.ac.jp>
2025-03-02 23:30:03 +0900

    Mod mu.py

--- iugonet/ground/mu.py
+++ iugonet/ground/mu.py
@@ -129,7 +129,8 @@ def mu(
 
                 if pr == 'org':
                     pathformat = '%Y/%Y%m/%Y%m%d.nc'
-                elif pr == 'wnd':
+                    pr = ''
+                 elif pr == 'wnd':
                     pathformat = '%Y/%Y%m/%Y%m%d.wnd.nc'
 
                 local_path = '/rish/misc/sgk/mu/' + dp + '/nc/'
